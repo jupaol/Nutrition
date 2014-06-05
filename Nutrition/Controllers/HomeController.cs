43de1750fcd7685266412cf9a964ddfb5bf3d38d
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2014-06-05 01:18:11 -0400

    Updating nugets, initializing ninject

--- Nutrition/Controllers/HomeController.cs
+++ Nutrition/Controllers/HomeController.cs
@@ -1,22 +1,23 @@
 ﻿using System.Web.Mvc;
+using Microsoft.Practices.ServiceLocation;
 using Nutrition.UI.Services;
 
 namespace Nutrition.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ISimpleComponent _simpleComponent;
+        private readonly IServiceLocator _serviceLocator;
 
-        public HomeController(ISimpleComponent simpleComponent)
+        public HomeController(IServiceLocator serviceLocator)
         {
-            _simpleComponent = simpleComponent;
+            _serviceLocator = serviceLocator;
         }
 
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
-            return View((object) _simpleComponent.Greeting());
+            return View((object) _serviceLocator.GetInstance<ISimpleComponent>().Greeting());
         }
     }
 }
---
+++ Nutrition/DI/ServiceLocatorModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Ninject.Modules;
+using NinjectAdapter;
+
+namespace Nutrition.DI
+{
+    public class ServiceLocatorModule : NinjectModule
+    {
+        public override void Load()
+        {
+            ServiceLocator.SetLocatorProvider(() => new NinjectServiceLocator(Kernel));
+
+            Kernel.Bind<IServiceLocator>().ToConstant(ServiceLocator.Current);
+        }
+    }
+}
