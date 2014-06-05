bc1672c9fccf7cd9dea6a2b6b8c49d546f278bec
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2014-06-05 01:07:39 -0400

    Adding Ninject

--- Nutrition/Controllers/HomeController.cs
+++ Nutrition/Controllers/HomeController.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Nutrition.UI.Services;
 
 namespace Nutrition.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISimpleComponent _simpleComponent;
+
+        public HomeController(ISimpleComponent simpleComponent)
+        {
+            _simpleComponent = simpleComponent;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
-            return View();
+            return View((object) _simpleComponent.Greeting());
         }
     }
 }
---
+++ Nutrition/DI/GeneralModule.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.Extensions.Conventions;
+using Ninject.Modules;
+using Nutrition.UI.Services;
+
+namespace Nutrition.DI
+{
+    public class GeneralModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind(config =>
+                config.From(
+                    typeof (GeneralModule).Assembly,
+                    typeof (ISimpleComponent).Assembly)
+                    .SelectAllClasses()
+                    .Where(type => !string.IsNullOrWhiteSpace(type.Namespace) && type.Namespace.StartsWith("Nutrition"))
+                    .BindAllInterfaces());
+
+            
+        }
+    }
+}
