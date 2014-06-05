78919c183360629f301f74746683bf81990bd0c6
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2014-06-05 18:21:33 -0400

    Adding slf4net adapter with NLog

--- Nutrition/Controllers/HomeController.cs
+++ Nutrition/Controllers/HomeController.cs
@@ -2,21 +2,26 @@
 using System.Web.Mvc;
 using Microsoft.Practices.ServiceLocation;
 using Nutrition.UI.Services;
+using slf4net;
 
 namespace Nutrition.Controllers
 {
     public partial class HomeController : Controller
     {
         private readonly IServiceLocator _serviceLocator;
+        private readonly ILogger _logger;
 
-        public HomeController(IServiceLocator serviceLocator)
+        public HomeController(IServiceLocator serviceLocator, ILogger logger)
         {
             _serviceLocator = serviceLocator;
+            _logger = logger;
         }
 
         public virtual ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            _logger.Trace("woooooooooooot" + DateTime.Now);
 
             return View((object) _serviceLocator.GetInstance<ISimpleComponent>().Greeting());
         }
---
+++ Nutrition/DI/LoggingInitializerModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using slf4net;
+
+namespace Nutrition.DI
+{
+    public class LoggingInitializerModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<ILogger>().ToMethod(x =>
+            {
+                if (
+                    x.Request != null &&
+                    x.Request.Target != null &&
+                    x.Request.Target.Member != null)
+                {
+                    return LoggerFactory.GetLogger(x.Request.Target.Member.ReflectedType);
+                }
+
+                return LoggerFactory.GetLogger(typeof(ILogger));
+            });
+        }
+    }
+}
